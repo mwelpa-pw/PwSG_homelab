bc46387b17f67b474be3f8c6e364426903de0441
mwelpa <maciej.welpa.stud@pw.edu.pl>
2026-03-25 20:16:01 +0100

    Add all source files

---
+++ resource.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define IDI_APPICON         101
+#define IDR_MENU            102
+#define IDR_ACCEL           103
+
+// Menu commands
+#define ID_FILE_EXPORT_BMP  201
+#define ID_FILE_SAVE_CSV    202
+#define ID_FILE_LOAD_CSV    203
+
+#define ID_GRADIENT_RESET   301
+#define ID_GRADIENT_LINEAR  302
+#define ID_GRADIENT_RADIAL  303
